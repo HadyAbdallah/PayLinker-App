b8b745078d4adec0dc51022258ef73f7f8025446
HadyAbdallah <ahady916@gmail.com>
2024-12-20 18:05:09 +0200

    Create order info item and total price widget

--- lib/Features/cheakout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/Features/cheakout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:paylinker/Features/cheakout/presentation/views/widgets/order_info_item.dart';
+import 'package:paylinker/Features/cheakout/presentation/views/widgets/total_price_widget.dart';
 
 class MyCartViewBody extends StatelessWidget {
   const MyCartViewBody({super.key});
@@ -6,9 +8,32 @@ class MyCartViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 18),
+      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 20),
       child: Column(
-        children: [Image.asset("assets/images/basket_image.png")],
+        children: [
+          Image.asset("assets/images/basket_image.png"),
+          SizedBox(
+            height: 25,
+          ),
+          OrderInfoItem(title: "Order Subtotal", value: r"$42.97"),
+          SizedBox(
+            height: 3,
+          ),
+          OrderInfoItem(title: "Discount", value: r"$0"),
+          SizedBox(
+            height: 3,
+          ),
+          OrderInfoItem(title: "Shipping", value: r"$8"),
+          SizedBox(
+            height: 3,
+          ),
+          Divider(
+            thickness: 2,
+            height: 34,
+            color: Color(0xffC7C7C7),
+          ),
+          TotalPriceWidget(title: 'Total', value: r'$50.97')
+        ],
       ),
     );
   }
